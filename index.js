d08b5acee41b98c4388f4bb41c87675227753767
Amitabh <amitabh10b26.hst21@gmail.com>
2025-10-25 10:40:38 +0530

    Fixed cors issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend port
+    origin: "https://social-frontend-fawn.vercel.app", // frontend port
     credentials: true,
   })
 );
